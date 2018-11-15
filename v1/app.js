cda9b48e54ceddb067b32a4489e3e1300b851a90
BackStreetSoy <jordanedwardobey@gmail.com>
2018-11-15 12:49:41 +0100

    removed unecessary doctype line

--- v1/app.js
+++ v1/app.js
@@ -5,7 +5,6 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
-<!DOCTYPE  html>
 app.get("/", function(req, res){
     res.render("landing")
 });
