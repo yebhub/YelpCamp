b4c93ce3b970f5b998baa86e20b13e8ac0ea58d1
BackStreetSoy <jordanedwardobey@gmail.com>
2018-11-17 12:49:26 +0100

    added new version to include mongoose

---
+++ v2/app.js
@@ -0,0 +1,72 @@
+var express        = require("express"),
+    app            = express(),
+    bodyParser     = require("body-parser"),
+    mongoose       = require("mongoose")
+
+
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true })
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+
+// SCHEMA SETUP
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://images.unsplash.com/photo-1525811902-f2342640856e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1a7383ad093ffea99d373681b9974056&auto=format&fit=crop&w=1502&q=80"}, function(err, campground){
+//             if(err){
+//                 console.log(err);
+//             }else{
+//                 console.log("new campground: ");
+//                 console.log(campground)
+//             }
+//         });
+
+
+app.get("/", function(req, res){
+    res.render("landing")
+});
+
+
+
+app.get("/campgrounds", function(req, res){
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        } else{
+             res.render("campgrounds", {campgrounds: campgrounds});
+        }
+    });
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    })
+
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs")
+
+});
+
+app.listen(3000, function(){
+    console.log("server started!!");
+});
