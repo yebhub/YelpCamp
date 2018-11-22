2453ea535dced047a88f1f9d774e402fc4dd3a57
BackStreetSoy <jordanedwardobey@gmail.com>
2018-11-22 16:25:36 -0500

    added styling

--- v5/app.js
+++ v5/app.js
@@ -7,12 +7,11 @@ var express        = require("express"),
     User           = require("./models/user"),
     SeedDB         = require("./seeds")
 
-
-SeedDB();
 mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', { useNewUrlParser: true })
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+SeedDB();
 
 
 app.get("/", function(req, res){
