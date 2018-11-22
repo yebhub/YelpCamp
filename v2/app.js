4f9514a00da850a08b30bd028404d4bf7ed426b7
BackStreetSoy <jordanedwardobey@gmail.com>
2018-11-22 12:11:14 -0500

    added new version of yelpcamp

--- v2/app.js
+++ v2/app.js
@@ -15,7 +15,7 @@ var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String,
     description: String
-})
+});
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
